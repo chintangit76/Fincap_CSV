dad197282c9b9b59d5ae64612d402642e5c0e59a
chintangit76 <chintangithub@gmail.com>
2025-05-24 19:37:10 +0530

    Read Page Design Change

--- FinCAP/FrmRead_Save_CSV.Designer.cs
+++ FinCAP/FrmRead_Save_CSV.Designer.cs
@@ -39,10 +39,9 @@
             // BtnProcess
             // 
             this.BtnProcess.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnProcess.Location = new System.Drawing.Point(39, 98);
-            this.BtnProcess.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnProcess.Location = new System.Drawing.Point(29, 80);
             this.BtnProcess.Name = "BtnProcess";
-            this.BtnProcess.Size = new System.Drawing.Size(133, 31);
+            this.BtnProcess.Size = new System.Drawing.Size(100, 25);
             this.BtnProcess.TabIndex = 0;
             this.BtnProcess.Text = "Process";
             this.BtnProcess.UseVisualStyleBackColor = true;
@@ -51,10 +50,9 @@
             // BtnClose
             // 
             this.BtnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnClose.Location = new System.Drawing.Point(546, 98);
-            this.BtnClose.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnClose.Location = new System.Drawing.Point(411, 80);
             this.BtnClose.Name = "BtnClose";
-            this.BtnClose.Size = new System.Drawing.Size(133, 31);
+            this.BtnClose.Size = new System.Drawing.Size(99, 25);
             this.BtnClose.TabIndex = 1;
             this.BtnClose.Text = "Close";
             this.BtnClose.UseVisualStyleBackColor = true;
@@ -65,31 +63,28 @@
             this.TxtMsg.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.TxtMsg.Location = new System.Drawing.Point(39, 153);
-            this.TxtMsg.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtMsg.Location = new System.Drawing.Point(29, 124);
             this.TxtMsg.Multiline = true;
             this.TxtMsg.Name = "TxtMsg";
             this.TxtMsg.ReadOnly = true;
-            this.TxtMsg.Size = new System.Drawing.Size(640, 155);
+            this.TxtMsg.Size = new System.Drawing.Size(481, 127);
             this.TxtMsg.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(39, 18);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(29, 15);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(349, 17);
+            this.label1.Size = new System.Drawing.Size(262, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "This Application will read the csv file from Input Folder.";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(39, 46);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(29, 37);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(654, 17);
+            this.label2.Size = new System.Drawing.Size(495, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Replace the Group Name value with Tally Name column in \"Group Master.csv\" file va" +
     "lue in Input folder.";
@@ -97,25 +92,23 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(39, 71);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(29, 58);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(282, 17);
+            this.label3.Size = new System.Drawing.Size(211, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "After change new file store in Output folder.";
             // 
             // FrmRead_Save_CSV
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(731, 321);
+            this.ClientSize = new System.Drawing.Size(548, 261);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.TxtMsg);
             this.Controls.Add(this.BtnClose);
             this.Controls.Add(this.BtnProcess);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FrmRead_Save_CSV";
             this.Text = "Change Group Name From CSV File";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
